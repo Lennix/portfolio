b837b6b031be86256f2ae0ad8a63cf4fe9b1f388
Pascal Kühne <pk@qlax.de>
2020-02-14 22:32:12 +0100

    Auch hier Kompatibilität zu alten Dokumenten herstellen (wurde bei Kauf schon gemacht)

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/INGDiBaExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/INGDiBaExtractor.java
@@ -162,7 +162,7 @@ public class INGDiBaExtractor extends AbstractPDFExtractor
 
                         .section("shares")
                         //
-                        .match("^Nominale St.ck (?<shares>[\\d.]+(,\\d+)?)")
+                        .match("^Nominale( St.ck)? (?<shares>[\\d.]+(,\\d+)?).*")
                         .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
 
                         .section("date") //
